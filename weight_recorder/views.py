4d0ec469c1b5e204ec3e537bdcfa331f5349a6a1
Cleyson Lima <cleysonph@gmail.com>
2020-02-24 03:00:05 -0300

    Added a chart in the dashboard

--- weight_recorder/views.py
+++ weight_recorder/views.py
@@ -8,10 +8,17 @@ from .models import Weight
 @login_required
 def dashboard(request):
     weight_list = Weight.objects.filter(insert_by=request.user)
+    weight_chart_data = {
+        'labels': [weight.weight_date.strftime("%m/%d/%Y") for weight in weight_list],
+        'data': [weight.weight_value for weight in weight_list]
+    }
+
+    print(weight_chart_data)
 
     context = {
         'title': 'Dashboard',
         'weight_list': weight_list,
+        'weight_chart_data': weight_chart_data,
     }
 
     return render(request, 'weight_recorder/dashboard.html', context)
