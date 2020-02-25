0ef28038006e4af2ba04bbb3a65dbf992f6eac01
Cleyson Lima <cleysonph@gmail.com>
2020-02-25 19:17:55 -0300

    Created the UserProfle Model in the accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from weight_recorder.views import weight_create
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    profile_image = models.ImageField(
+        verbose_name="Foto de perfil", upload_to="profile_image/%d/%m/%Y", blank=True)
+    height = models.FloatField("Altura", blank=True)
+    weight_goal = models.FloatField("Meta de peso", blank=True)
+    user = models.ForeignKey(
+        "auth.User", verbose_name="Usuário", on_delete=models.CASCADE, blank=False)
