6386cbcffe7db9d9875ccf85ae657ab20fe0de15
Cleyson Lima <cleysonph@gmail.com>
2020-02-25 17:55:52 -0300

    Create the urls for the accounts app

--- django_weight_recorde/urls.py
+++ django_weight_recorde/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("weight_recorder.urls", namespace="weight_recorder")),
+    path("conta/", include("accounts.urls", namespace="accounts")),
     path('admin/', admin.site.urls),
 ]
