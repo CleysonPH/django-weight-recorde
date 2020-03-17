385874b12e8cecfc53b77e6f63f892b76ef3fd8a
Cleyson Lima <cleysonph@gmail.com>
2020-03-16 23:23:15 -0300

    Create the tests for weight_edit view in weight_recorder app

--- weight_recorder/tests/test_views.py
+++ weight_recorder/tests/test_views.py
@@ -111,7 +111,7 @@ class WeightCreateViewTest(TestCase):
         }
 
     def test_redirect_if_not_logged_in(self):
-        response = self.client.get('/dashboard')
+        response = self.client.get('/peso/adicionar')
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response.url.startswith('/conta/signin'))
 
@@ -139,3 +139,69 @@ class WeightCreateViewTest(TestCase):
         response = self.client.get(reverse('weight_recorder:weight_create'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'weight_recorder/weight_form.html')
+
+
+class WeightEditViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create_user(
+            username="TestUser", password="TestPassword")
+
+        another_test_user = User.objects.create_user(
+            username="AnotherTestUser", password="AnotherTestPassword")
+
+        number_of_weights = 10
+        for _ in range(number_of_weights):
+            Weight.objects.create(
+                weight_value=65.1, weight_date=datetime.date.today(), insert_by=test_user)
+
+        for _ in range(number_of_weights):
+            Weight.objects.create(
+                weight_value=65.1, weight_date=datetime.date.today(), insert_by=another_test_user)
+
+    def setUp(self):
+        self.test_user = {
+            'username': 'TestUser',
+            'password': 'TestPassword',
+        }
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get('/peso/1/editar')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith('/conta/signin'))
+
+    def test_view_url_exists_at_desired_location(self):
+        login = self.client.login(
+            username=self.test_user['username'],
+            password=self.test_user['password'],
+        )
+        response = self.client.get('/peso/1/editar')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        login = self.client.login(
+            username=self.test_user['username'],
+            password=self.test_user['password'],
+        )
+        response = self.client.get(
+            reverse('weight_recorder:weight_edit', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        login = self.client.login(
+            username=self.test_user['username'],
+            password=self.test_user['password'],
+        )
+        response = self.client.get(
+            reverse('weight_recorder:weight_edit', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'weight_recorder/weight_form.html')
+
+    def test_if_not_found_for_edit_a_weight_insert_by_another_user(self):
+        login = self.client.login(
+            username=self.test_user['username'],
+            password=self.test_user['password'],
+        )
+        response = self.client.get(
+            reverse('weight_recorder:weight_edit', kwargs={'pk': 15}))
+        self.assertEqual(response.status_code, 404)
