0a361d475d5ab386856a539deece022a3fb5b973
Cleyson Lima <cleysonph@gmail.com>
2020-03-16 23:15:51 -0300

    Create the tests for weight_create view in weight_recorder app

--- weight_recorder/tests/test_views.py
+++ weight_recorder/tests/test_views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from weight_recorder.models import Weight
 
 
-class DashboardView(TestCase):
+class DashboardViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         test_user = User.objects.create_user(
@@ -33,6 +33,7 @@ class DashboardView(TestCase):
     def test_redirect_if_not_logged_in(self):
         response = self.client.get('/dashboard')
         self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith('/conta/signin'))
 
     def test_view_url_exists_at_desired_location(self):
         login = self.client.login(
@@ -50,7 +51,7 @@ class DashboardView(TestCase):
         response = self.client.get(reverse('weight_recorder:dashboard'))
         self.assertEqual(response.status_code, 200)
 
-    def test_view_uses_correct_location(self):
+    def test_view_uses_correct_template(self):
         login = self.client.login(
             username=self.test_user['username'],
             password=self.test_user['password'],
@@ -83,3 +84,58 @@ class DashboardView(TestCase):
 
         for weight in weights:
             self.assertEqual(response.context['user'], weight.insert_by)
+
+
+class WeightCreateViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create_user(
+            username="TestUser", password="TestPassword")
+
+        another_test_user = User.objects.create_user(
+            username="AnotherTestUser", password="AnotherTestPassword")
+
+        number_of_weights = 10
+        for _ in range(number_of_weights):
+            Weight.objects.create(
+                weight_value=65.1, weight_date=datetime.date.today(), insert_by=test_user)
+
+        for _ in range(number_of_weights):
+            Weight.objects.create(
+                weight_value=65.1, weight_date=datetime.date.today(), insert_by=another_test_user)
+
+    def setUp(self):
+        self.test_user = {
+            'username': 'TestUser',
+            'password': 'TestPassword',
+        }
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get('/dashboard')
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith('/conta/signin'))
+
+    def test_view_url_exists_at_desired_location(self):
+        login = self.client.login(
+            username=self.test_user['username'],
+            password=self.test_user['password'],
+        )
+        response = self.client.get('/peso/adicionar')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        login = self.client.login(
+            username=self.test_user['username'],
+            password=self.test_user['password'],
+        )
+        response = self.client.get(reverse('weight_recorder:weight_create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        login = self.client.login(
+            username=self.test_user['username'],
+            password=self.test_user['password'],
+        )
+        response = self.client.get(reverse('weight_recorder:weight_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'weight_recorder/weight_form.html')
