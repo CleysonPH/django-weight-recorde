741cca9c42e7090ec458a5cb8ab0f98fa5565a14
Cleyson Lima <cleysonph@gmail.com>
2020-03-17 22:59:47 -0300

    Create tests for update_password view in accounts app

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -194,3 +194,73 @@ class UserProfileUpdateViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(
             response, 'accounts/user_profile_form.html')
+
+
+class UserProfileUpdateViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user = User.objects.create_user(
+            username='TestUser', password='TestPassword')
+        user_profile = UserProfile.objects.create(
+            height=1.75, weight_goal=65.5, user=test_user)
+
+    def setUp(self):
+        self.test_user_credentials = {
+            'username': 'TestUser',
+            'password': 'TestPassword',
+        }
+
+        self.test_user = User.objects.get(username='TestUser')
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get('/conta/senha/alterar')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url, f"{reverse('accounts:signin')}?next=/conta/senha/alterar")
+
+    def test_view_url_exists_at_desired_location(self):
+        login = self.client.login(
+            username=self.test_user_credentials['username'],
+            password=self.test_user_credentials['password'],
+        )
+        response = self.client.get('/conta/senha/alterar')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        login = self.client.login(
+            username=self.test_user_credentials['username'],
+            password=self.test_user_credentials['password'],
+        )
+        response = self.client.get(reverse('accounts:update_password'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        login = self.client.login(
+            username=self.test_user_credentials['username'],
+            password=self.test_user_credentials['password'],
+        )
+        response = self.client.get(reverse('accounts:update_password'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'accounts/update_password.html')
+
+    def test_redirects_to_dashboard_on_success(self):
+        login = self.client.login(
+            username=self.test_user_credentials['username'],
+            password=self.test_user_credentials['password'],
+        )
+        response = self.client.post(reverse('accounts:update_password'), {
+                                    'old_password': 'TestPassword', 'new_password1': 'NewTestPassword', 'new_password2': 'NewTestPassword'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('weight_recorder:dashboard'))
+
+    def test_if_password_really_change(self):
+        login = self.client.login(
+            username=self.test_user_credentials['username'],
+            password=self.test_user_credentials['password'],
+        )
+        old_password = self.test_user.password
+        response = self.client.post(reverse('accounts:update_password'), {
+                                    'old_password': 'TestPassword', 'new_password1': 'NewTestPassword', 'new_password2': 'NewTestPassword'}, follow=True)
+        new_password = response.context['user'].password
+        self.assertTrue(old_password != new_password)
